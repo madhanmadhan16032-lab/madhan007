728f4ad12d24906bc6485689df1e8a35f5e91759
madhanmadhan16032-lab <madhanmadhan16032@gmail.com>
2026-09-11 23:44:55 +0530

    Update employee_salary_analyzer_v5.py

--- employee_salary_analyzer_v5.py
+++ employee_salary_analyzer_v5.py
@@ -17,7 +17,79 @@ employees = [
                 "salary": 108000,
                 "bonus": 10000,
             }
-        ]
+    ]
+
+def view_employees():
+     for i in employees:
+         print("\n",i)
+
+def search_employee():
+        print("---------employees details--------")
+        name = input("enter your name:")
+        found = False
+
+        for n in employees:
+            if n["name"].lower() == name.lower():
+                print("employee founded")
+                print("name:", n["name"])
+                print("department:", n["dep"])
+                print("salary:", n["salary"])
+                print("Bonus:", n["bonus"])
+                found = True
+
+        if found == False:
+            print("not found")
+
+def salary_analysis():
+        print("===========salary analysis===========")
+        print("total employees:", len(employees))
+        total_salary = 0
+        for i in employees:
+            total = i["salary"] + i["bonus"]
+            total_salary = total_salary + total
+        print("total salary:", total_salary)
+
+        average_salary = total_salary / len(employees)
+        print("average_salary:", average_salary)
+
+        hsalaries = []
+
+        for h in employees:
+            hsalaries.append(h["salary"])
+
+        highest_salary = max(hsalaries)
+        print(highest_salary)
+
+        lsalaries = []
+
+        for l in employees:
+            lsalaries.append(l["salary"])
+
+        lowest_salary = min(lsalaries)
+        print(lowest_salary)
+
+def add_employees():
+        print("-----------add employees----------")
+
+        name = input("enter the employee name:")
+        department = input("enter their department:")
+        salary = float(input("enter their salary:"))
+        bonus = float(input("enter their bonus:"))
+
+        add_new_employee = {
+            "name": name,
+            "dep": department,
+            "salary": salary,
+            "bonus": bonus
+        }
+
+        employees.append(add_new_employee)
+
+def delete_employee():
+    name=input("enter the name if you want : ")
+    for n in employees:
+        if n["name"].lower() == name.lower():
+            employees.remove(n)
 
 while True:
     print("\n-------------employee salary----------")
@@ -25,80 +97,29 @@ while True:
     print("2.Search Employee")
     print("3.Salary Analysis")
     print("4.Add Employee")
-    print("5.Exit")
+    print("5.Delete Employee")
+    print("6.Exit")
 
     choice=input("enter your number here:")
 
     if choice=="1":
-        for i in employees:
-            print(i)
+        view_employees()
 
     elif choice=="2":
-        print("---------employees details--------")
-        name=input("enter your name:")
-        found = False
-
-        for n in employees:
-            if n["name"].lower()==name.lower():
-                print("employee founded")
-                print("name:",n["name"])
-                print("department:",n["dep"])
-                print("salary:",n["salary"])
-                print("Bonus:",n["bonus"])
-                found = True
-
-        if found == False:
-                 print("not found")
-
+        search_employee()
 
     elif choice=="3":
-       print("===========salary analysis===========")
-       print("total employees:",len(employees))
-       total_salary=0
-       for i in employees:
-           total=i["salary"] + i["bonus"]
-           total_salary=total_salary+total
-       print("total salary:",total_salary)
-
-       average_salary=total_salary/len(employees)
-       print("average_salary:",average_salary)
-
-       hsalaries=[]
-
-       for h in employees:
-           hsalaries.append(h["salary"])
-
-       highest_salary=max(hsalaries)
-       print(highest_salary)
-
-       lsalaries=[]
-
-       for l in employees:
-           lsalaries.append(l["salary"])
-
-       lowest_salary=min(lsalaries)
-       print(lowest_salary)
+        salary_analysis()
 
     elif choice=="4":
-        print("-----------add employees----------")
-
-        name=input("enter the employee name:")
-        department=input("enter their department:")
-        salary=input("enter their salary:")
-        bonus=input("enter their bonus:")
-
-        add_new_employee={
-            "name":name,
-            "dep":department,
-            "salary":salary,
-            "bonus":bonus
-        }
-
-        employees.append(add_new_employee)
+        add_employees()
 
     elif choice=="5":
-        print("exited")
+        delete_employee()
+
+    elif choice=="6":
+        print("thanks for this app ")
         break
 
     else:
-        print("invail number")
+        print("invaild code")
